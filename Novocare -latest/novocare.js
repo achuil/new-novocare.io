a03638564da9ebb6a3dcce30d7c2271e92dd3c1a
achuil <53069335+achuil@users.noreply.github.com>
2019-08-01 04:13:08 +0000

    Initial commit

---
+++ Novocare -latest/novocare.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "80px";
+document.getElementsByTagName("body")[0].style.fontSize = "40px";
+document.getElementsByTagName("nav-item")[1].style.bold.fontSize = "100px";
